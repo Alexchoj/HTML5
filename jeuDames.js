a117de9ce596e5e6dbf7b8c702e4216aae8de002
Alexchoj <alexchoj@hotmail.fr>
2017-04-09 22:50:35 +0200

    Mouseover et mouseout OK ! Click commencés
    PB ligne 302, le test ne passe pas.

--- jeuDames.js
+++ jeuDames.js
@@ -24,55 +24,339 @@ $(document).ready(function(){
 	$("#plateau").append(html);
 
 	$('[class^="pionNoir"]').on("mouseover", function(){ 
-		
-		var i=Number($(this).attr("class")[8]);
+		var i=Number($(this).attr("class")[8]);			
 		var j=Number($(this).attr("class")[9]);
+		var k=Number($(this).attr("class")[10]);
 
-		$('#'+i+''+j).addClass("vert");
+	
+		if(k==0){
 
-		if (($("#"+(i+1)+""+(j+1)).children().length<1)){
-			$("#"+(i+1)+""+(j+1)).addClass("vert");
-		}
-		if (($("#"+(i+1)+""+(j-1)).children().length<1)){
-			$("#"+(i+1)+""+(j-1)).addClass("vert");
+
+			//j=Number($(this).attr("class")[11]);
+			if (($("#"+(i+1)+"9").children().length<1)){
+				$("#"+(i+1)+"9").addClass("vert");
+			}
+			
+
+
+			$('#'+i+"10").addClass("vert");
+		}else if (j==1){
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vert");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vert");
+			}
+			
+			$('#'+i+''+j).addClass("vert");
+
+		}else if (i==1){
+
+
+			$("#"+i+j).addClass("vert");
+			if (($("#"+(i+1)+''+(j+1)).children().length<1)){
+				$("#"+(i+1)+''+(j+1)).addClass("vert");
+			}
+			if (($("#"+(i+1)+''+(j-1)).children().length<1)){
+				$("#"+(i+1)+''+(j1)).addClass("vert");
+			}
+		} else {
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vert");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vert");
+			}
+
+
+			$("#"+(i)+""+(j)).addClass("vert");
+
 		}
 	});
 
 	$('[class^="pionBlanc"]').on("mouseover", function(){ 
-		var i=Number($(this).attr("class")[9]);
+		var i=Number($(this).attr("class")[9]);			
 		var j=Number($(this).attr("class")[10]);
+		var k=Number($(this).attr("class")[11]);
 
-		$('#'+i+''+j).addClass("rouge");
 
-		if (($("#"+(i-1)+""+(j+1)).children().length<1)){
-			$("#"+(i-1)+""+(j+1)).addClass("rouge");
+	
+		if (k==0){
+
+
+			if (($("#"+(i-1)+"9").children().length<1)){
+				$("#"+(i-1)+"9").addClass("rouge");
+			}
+			
+			$('#'+i+''+j+'0').addClass("rouge");
+
+		}else if(j==1){
+
+
+
+			j=Number($(this).attr("class")[11]);
+			if (($("#"+"9"+(j-1)).children().length<1)){
+				$("#"+"9"+(j-1)).addClass("rouge");
+			}
+			
+			if (($("#"+"9"+(j+1)).children().length<1)){
+				$("#"+"9"+(j+1)).addClass("rouge");
+			}
+
+
+			$('#'+i+'1').addClass("rouge");
+		}else if (j==0 && k%2!=0){
+
+
+
+			$("#"+"10"+k).addClass("rouge");
+			if (($("#"+"9"+(k-1)).children().length<1)){
+				$("#"+"9"+(k-1)).addClass("rouge");
+			}
+			if (($("#"+"9"+(k+1)).children().length<1)){
+				$("#"+"9"+(k+1)).addClass("rouge");
+			}
+		} else {
+
+
+
+			if (($("#"+(i-1)+""+(j+1)).children().length<1)){
+				$("#"+(i-1)+""+(j+1)).addClass("rouge");
+			}
+			if (($("#"+(i-1)+""+(j-1)).children().length<1)){
+				$("#"+(i-1)+""+(j-1)).addClass("rouge");
+			}
+
+
+			$("#"+(i)+""+(j)).addClass("rouge");
+
 		}
-		if (($("#"+(i-1)+""+(j-1)).children().length<1)){
-			$("#"+(i-1)+""+(j-1)).addClass("rouge");
-		}
+
 	});
 
 	$('[class^="pionNoir"]').on("mouseout", function(){ 
-		
-		var i=Number($(this).attr("class")[8]);
+		var i=Number($(this).attr("class")[8]);			
 		var j=Number($(this).attr("class")[9]);
+		var k=Number($(this).attr("class")[10]);
 
-		$('#'+i+''+j).removeClass("vert");
+	
+		if(k==0){
 
-		$("#"+(i+1)+""+(j+1)).removeClass("vert");
-		$("#"+(i+1)+""+(j-1)).removeClass("vert");
+			if (($("#"+(i+1)+"9").children().length<1)){
+				$("#"+(i+1)+"9").removeClass("vert");
+			}
+			
+
+
+			$('#'+i+"10").removeClass("vert");
+		}else if (j==1){
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).removeClass("vert");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).removeClass("vert");
+			}
+			
+			$('#'+i+''+j).removeClass("vert");
+
+		}else if (i==1){
+
+
+
+			$("#"+i+j).removeClass("vert");
+			if (($("#"+(i+1)+''+(j+1)).children().length<1)){
+				$("#"+(i+1)+''+(j+1)).removeClass("vert");
+			}
+			if (($("#"+(i+1)+''+(j-1)).children().length<1)){
+				$("#"+(i+1)+''+(j-1)).removeClass("vert");
+			}
+		} else {
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).removeClass("vert");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).removeClass("vert");
+			}
+
+
+			$("#"+(i)+""+(j)).removeClass("vert");
+
+		}
 	});
 
 	$('[class^="pionBlanc"]').on("mouseout", function(){ 
-		
-		var i=Number($(this).attr("class")[9]);
+		var i=Number($(this).attr("class")[9]);			
 		var j=Number($(this).attr("class")[10]);
+		var k=Number($(this).attr("class")[11]);
+	
+		if (k==0){
 
-		$('#'+i+''+j).removeClass("rouge");
+			if (($("#"+(i-1)+"9").children().length<1)){
+				$("#"+(i-1)+"9").removeClass("rouge");
+			}
+			
+			$('#'+i+''+j+'0').removeClass("rouge");
 
-		$("#"+(i-1)+""+(j+1)).removeClass("rouge");
-		$("#"+(i-1)+""+(j-1)).removeClass("rouge");
+		}else if(j==1){
+
+			j=Number($(this).attr("class")[11]);
+			if (($("#"+"9"+(j-1)).children().length<1)){
+				$("#"+"9"+(j-1)).removeClass("rouge");
+			}
+			
+			if (($("#"+"9"+(j+1)).children().length<1)){
+				$("#"+"9"+(j+1)).removeClass("rouge");
+			}
+
+
+			$('#'+i+'1').removeClass("rouge");
+		}else if (j==0 && k%2!=0){
+
+			$("#"+"10"+k).removeClass("rouge");
+			if (($("#"+"9"+(k-1)).children().length<1)){
+				$("#"+"9"+(k-1)).removeClass("rouge");
+			}
+			if (($("#"+"9"+(k+1)).children().length<1)){
+				$("#"+"9"+(k+1)).removeClass("rouge");
+			}
+		} else {
+
+			if (($("#"+(i-1)+""+(j+1)).children().length<1)){
+				$("#"+(i-1)+""+(j+1)).removeClass("rouge");
+			}
+			if (($("#"+(i-1)+""+(j-1)).children().length<1)){
+				$("#"+(i-1)+""+(j-1)).removeClass("rouge");
+			}
+
+
+			$("#"+(i)+""+(j)).removeClass("rouge");
+
+		}
+
+
 	});
 
 
+	$('[class^="pionNoir"]').on("click", function(){ 
+		$(this).parent().removeClass("vert");
+		//$('[class^="vertLock"]').removeClass("vertLock");
+		$('[class^="selected"]').removeClass("selected");;
+		$(this).parent().addClass("selected");
+		
+		var i=Number($(this).attr("class")[8]);			
+		var j=Number($(this).attr("class")[9]);
+		var k=Number($(this).attr("class")[10]);
+
+
+
+	
+		if(k==0){
+
+			if (($("#"+(i+1)+"9").children().length<1)){
+				$("#"+(i+1)+"9").addClass("vertLock");
+			}
+			
+
+		}else if (j==1){
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vertLock");
+			}
+
+		}else if (i==1){
+
+
+			if (($("#"+(i+1)+''+(j+1)).children().length<1)){
+				$("#"+(i+1)+''+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+''+(j-1)).children().length<1)){
+				$("#"+(i+1)+''+(j-1)).addClass("vertLock");
+			}
+		} else {
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vertLock");
+			}
+		}
+
+		
+		
+	});
+	
+	$('[class^="vertLock"]').on("mouseover", function(){ 
+		console.log("yes");
+		$(this).parent().addClass('[class^="pionNoir"]');
+	});
+
+	$('[class^="pionBlanc"]').on("click", function(){ 
+
+		$(this).parent().removeClass("rouge");
+		//$('[class^="vertLock"]').removeClass("vertLock");
+		$('[class^="selected"]').removeClass("selected");;
+		$(this).parent().addClass("selected");
+		/*
+		var i=Number($(this).attr("class")[8]);			
+		var j=Number($(this).attr("class")[9]);
+		var k=Number($(this).attr("class")[10]);
+
+
+		
+	
+		if(k==0){
+
+			if (($("#"+(i+1)+"9").children().length<1)){
+				$("#"+(i+1)+"9").addClass("vertLock");
+			}
+			
+
+		}else if (j==1){
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vertLock");
+			}
+
+		}else if (i==1){
+
+
+			if (($("#"+(i+1)+''+(j+1)).children().length<1)){
+				$("#"+(i+1)+''+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+''+(j-1)).children().length<1)){
+				$("#"+(i+1)+''+(j-1)).addClass("vertLock");
+			}
+		} else {
+
+
+			if (($("#"+(i+1)+""+(j+1)).children().length<1)){
+				$("#"+(i+1)+""+(j+1)).addClass("vertLock");
+			}
+			if (($("#"+(i+1)+""+(j-1)).children().length<1)){
+				$("#"+(i+1)+""+(j-1)).addClass("vertLock");
+			}
+		}
+		*/
+		
+	});
+
+	
 });
